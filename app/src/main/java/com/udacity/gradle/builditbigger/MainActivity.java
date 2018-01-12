61a2d02d3bcdf57085221f8cb3c25187faee9b1e
Shane Richards <shane.richards121@gmail.com>
2018-01-11 22:09:08 -0500

    app uses android library to display jokes

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,13 +1,14 @@
 package com.udacity.gradle.builditbigger;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Toast;
 
 import com.shane.jokegenerator.JokeGenerator;
+import com.shane.joketeller.JokeActivity;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -44,6 +45,9 @@ public class MainActivity extends AppCompatActivity {
     public void tellJoke(View view) {
         JokeGenerator jokeGenerator = new JokeGenerator();
         String joke = jokeGenerator.getJoke();
-        Toast.makeText(this, joke, Toast.LENGTH_SHORT).show();
+
+        Intent intent = new Intent(MainActivity.this, JokeActivity.class);
+        intent.putExtra(JokeActivity.EXTRA_JOKE, joke);
+        startActivity(intent);
     }
 }
